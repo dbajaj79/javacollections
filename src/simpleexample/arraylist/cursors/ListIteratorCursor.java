2f1aec528d870d1d5dc79972b2b016a86db26777
deepakappways <deepakb@waysapp.in>
2019-07-01 10:42:47 +0530

    Remove Element and Remove Odd Numbers from ArrayList using Iterator

--- src/simpleexample/arraylist/cursors/ListIteratorCursor.java
+++ src/simpleexample/arraylist/cursors/ListIteratorCursor.java
@@ -1,4 +1,68 @@
 package simpleexample.arraylist.cursors;
 
+import java.util.ArrayList;
+import java.util.ListIterator;
+
+/*
+* ListIterator
+* 1. version 1.2
+* 2. list classes
+* 3. not universal customer
+* 4. listIterator
+* 5. 9 Methods
+* 6. bi Directional
+* 7. all operations
+* 8. interface
+* 9.normal and generic
+* Importent Points
+* hasPrevious is Worked only if Cursor move to last index
+* else if will not Work
+*
+* */
 public class ListIteratorCursor {
+    public static void main(String[] args) {
+        //Normal Version
+        ArrayList arrayList = new ArrayList();
+        arrayList.add("A");
+        arrayList.add("B");
+        arrayList.add("C");
+        arrayList.add("C");
+        arrayList.add("D");
+        arrayList.add("E");
+        ListIterator listIterator = arrayList.listIterator();
+        while (listIterator.hasNext())
+        {
+            String value = (String)listIterator.next();
+            System.out.println(value);
+        }
+        //Previous Operation
+        while (listIterator.hasPrevious())
+        {
+            String value = (String)listIterator.previous();
+            System.out.println(value);
+        }
+
+        //Generic Version
+        ArrayList arrayList1 = new ArrayList();
+        arrayList.add("A");
+        arrayList.add("B");
+        arrayList.add("C");
+        arrayList.add("C");
+        arrayList.add("D");
+        arrayList.add("E");
+        ListIterator<String> listIterator1 = arrayList1.listIterator();
+        while (listIterator1.hasNext())
+        {
+            String value =listIterator1.next();
+            System.out.println(value);
+        }
+        //Previous Operation
+        while (listIterator1.hasPrevious())
+        {
+            String value = listIterator1.previous();
+            System.out.println(value);
+        }
+
+
+    }
 }
---
+++ src/simpleexample/arraylist/cursors/examplesIteratorCursor/RemoveElement.java
@@ -0,0 +1,25 @@
+package simpleexample.arraylist.cursors.examplesIteratorCursor;
+
+import java.util.Iterator;
+import java.util.Vector;
+
+public class RemoveElement {
+
+    public static void main(String[] args) {
+        Vector<String> strings = new Vector<>();
+        strings.add("A");
+        strings.add("B");
+        strings.addElement("C");
+
+        Iterator<String>iterator = strings.iterator();
+        while (iterator.hasNext())
+        {
+            if(iterator.next().equals("B"))
+            {
+                iterator.remove();
+            }
+        }
+        System.out.println(strings);
+
+    }
+}
