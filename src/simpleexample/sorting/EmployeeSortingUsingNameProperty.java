f811f2cb88789c474a51f5dabc381692b4c33eeb
deepakappways <deepakb@waysapp.in>
2019-07-02 11:03:26 +0530

    Implement Comparable using Name property for Employee

---
+++ src/simpleexample/sorting/EmployeeSortingUsingNameProperty.java
@@ -0,0 +1,20 @@
+package simpleexample.sorting;
+
+import simpleexample.model.Empolyee1;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class EmployeeSortingUsingNameProperty {
+
+    public static void main(String[] args) {
+        ArrayList<Empolyee1>empolyee1s =new ArrayList<>();
+        empolyee1s.add(new Empolyee1(112,"Z"));
+        empolyee1s.add(new Empolyee1(201,"D"));
+        empolyee1s.add(new Empolyee1(245,"H"));
+        empolyee1s.add(new Empolyee1(670,"J"));
+        System.out.println("EmployeeList without Sorting:"+empolyee1s);
+        Collections.sort(empolyee1s);
+        System.out.println("Employee After Sorting:"+empolyee1s);
+    }
+}
