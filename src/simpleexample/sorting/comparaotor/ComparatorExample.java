b411af4103c81eb37db3f0a465576b870c078b45
deepakappways <deepakb@waysapp.in>
2019-07-02 13:33:56 +0530

    Haset and LinkedSet Examples Done

--- src/simpleexample/sorting/comparaotor/ComparatorExample.java
+++ src/simpleexample/sorting/comparaotor/ComparatorExample.java
@@ -12,23 +12,22 @@ import java.util.Collections;
 public class ComparatorExample {
 
     public static void main(String[] args) {
-        ArrayList<Color>colors = new ArrayList<>();
-        colors.add(new Color(1,"Yellow"));
-        colors.add(new Color(25,"Red"));
-        colors.add(new Color(20,"Green"));
-        colors.add(new Color(2,"Purple"));
-        colors.add(new Color(5,"Alfa"));
-        colors.add(new Color(0,"Aqua"));
-        System.out.println("Colors Without Sorting"+colors);
-        Collections.sort(colors,new ColorIdCompartor());
-        System.out.println("colors Sorting based on Id"+colors);
-        Collections.sort(colors,new ColorNameComparator());
-        System.out.println("Colors Sorting based on Name:"+colors);
-        Collections.sort(colors,new ColoarIdReverseComparator());
-        System.out.println("Colors Sorting Based on Id in Reverse Order:"+colors);
+        ArrayList<Color> colors = new ArrayList<>();
+        colors.add(new Color(1, "Yellow"));
+        colors.add(new Color(25, "Red"));
+        colors.add(new Color(20, "Green"));
+        colors.add(new Color(2, "Purple"));
+        colors.add(new Color(5, "Alfa"));
+        colors.add(new Color(0, "Aqua"));
+        System.out.println("Colors Without Sorting" + colors);
+        Collections.sort(colors, new ColorIdCompartor());
+        System.out.println("colors Sorting based on Id" + colors);
+        Collections.sort(colors, new ColorNameComparator());
+        System.out.println("Colors Sorting based on Name:" + colors);
+        Collections.sort(colors, new ColoarIdReverseComparator());
+        System.out.println("Colors Sorting Based on Id in Reverse Order:" + colors);
         Collections.sort(colors, new ColorNameReverseComparator());
-        System.out.println("Colors Sorting Based on Name in Reverse Order:"+colors);
-
+        System.out.println("Colors Sorting Based on Name in Reverse Order:" + colors);
 
 
     }
